ebc393e690bb66896306d4aff6fc6cdfaaaa463c
Ivan Morozko <Ivan.Morozko@oktetlabs.ru>
2023-11-21 22:02:33 +0400

    fix shellcheck warnings

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TOPLEVEL=`git rev-parse --show-toplevel`
+TOPLEVEL=$(git rev-parse --show-toplevel)
 
 function fail_no_command {
   echo "Failed to find ${1} in your \$PATH."
@@ -15,9 +15,9 @@ if ! command -v clang-format >/dev/null; then
   fail_no_command clang-format
 fi
 
-pushd $TOPLEVEL >/dev/null
-format_fixes=`git diff -U0 --no-color --staged | clang-format-diff.py -p1`
-popd >/dev/null
+pushd "$TOPLEVEL" >/dev/null || exit
+format_fixes=$(git diff -U0 --no-color --staged | clang-format-diff.py -p1)
+popd >/dev/null || exit
 
 if [ -z "${format_fixes}" ]; then
   exit 0
@@ -25,12 +25,12 @@ fi
 
 echo "${format_fixes}" | colordiff
 
-while read -p "Format issues detected by clang-format. Apply suggested fixes (yes/no)?" yn; do
+while read -r -p "Format issues detected by clang-format. Apply suggested fixes (yes/no)?" yn; do
   case $yn in
     [Yy]* )
-      pushd $TOPLEVEL >/dev/null
+      pushd "$TOPLEVEL" >/dev/null || exit
       echo "${format_fixes}" | git apply -p0 --index
-      popd >/dev/null
+      popd >/dev/null || exit
       break
       ;;
     [Nn]* ) exit ;;
