a01b690701f234d1cab49aa2fc8db0f58aa9ce81
Ivan Morozko <Ivan.Morozko@oktetlabs.ru>
2023-11-21 21:55:52 +0400

    minor style fixes

--- 03.Fractal/src/image.c
+++ 03.Fractal/src/image.c
@@ -9,6 +9,7 @@ image_p
 create_image(pixel_coord width, pixel_coord height)
 {
   image_t* v = malloc(sizeof(image_t));
+
   v->width = width;
   v->height = height;
   v->data = malloc(sizeof(pixel_data) * width * height);
@@ -23,24 +24,21 @@ free_image(image_p picture)
   free(picture);
 }
 
-unsigned int
-get_width(image_p picture)
-{
-  return picture->width;
-}
-
 void
 save_pgm(image_p picture, const char* filename)
 {
   FILE* to = fopen(filename, "w");
+  pixel_data* p = picture->data;
+
   assert_nequal(to, NULL);
 
   fprintf(to, "P2\n%u %u\n255\n", picture->width, picture->height);
 
-  pixel_data* p = picture->data;
-  for( pixel_coord y = 0; y < picture->height; ++y )
-    for( pixel_coord x = 0; x < picture->width; ++x )
+  for( pixel_coord y = 0; y < picture->height; ++y ) {
+    for( pixel_coord x = 0; x < picture->width; ++x ) {
       fprintf(to, "%u%c", *(p++), x == picture->width - 1 ? '\n' : ' ');
+    }
+  }
 
   fclose(to);
 }
@@ -64,21 +62,26 @@ void
 set_pixel(image_p picture, pixel_coord x, pixel_coord y, pixel_data color)
 {
   assert_dimensions(picture, x, y);
+
   picture->data[picture->width * y + x] = color;
 }
 
 void
 fill_random(image_p picture)
 {
-  for( pixel_coord y = 0; y < picture->height; ++y )
-    for( pixel_coord x = 0; x < picture->width; ++x )
+  for( pixel_coord y = 0; y < picture->height; ++y ) {
+    for( pixel_coord x = 0; x < picture->width; ++x ) {
       set_pixel(picture, x, y, (pixel_data)rand());
+    }
+  }
 }
 
 void
 fill_white(image_p picture)
 {
-  for( pixel_coord y = 0; y < picture->height; ++y )
-    for( pixel_coord x = 0; x < picture->width; ++x )
+  for( pixel_coord y = 0; y < picture->height; ++y ) {
+    for( pixel_coord x = 0; x < picture->width; ++x ) {
       set_pixel(picture, x, y, (pixel_data)0xff);
+    }
+  }
 }
