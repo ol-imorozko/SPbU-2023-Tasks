8f3e87fae9e5935061f68e3d911f0328c1a62c95
Ivan Morozko <Ivan.Morozko@oktetlabs.ru>
2023-11-21 21:50:49 +0400

    remove pointless comments

--- 03.Fractal/src/fractal.c
+++ 03.Fractal/src/fractal.c
@@ -7,22 +7,17 @@
 void
 draw_sierpinski_carpet(image_p picture, int x, int y, int size)
 {
-  if( size < 1 ) {
+  if( size < 1 )
     return;
-  }
 
-  // Calculate the size of the smaller squares
   int newSize = size / 3;
 
-  // Iterate through the 3x3 grid
   for( int i = 0; i < 3; i++ ) {
     for( int j = 0; j < 3; j++ ) {
-      // Skip the middle square
       if( i == 1 && j == 1 ) {
         for( int k = y + newSize; k < y + 2 * newSize; k++ ) {
           for( int l = x + newSize; l < x + 2 * newSize; l++ ) {
-            set_pixel(picture, l, k,
-                      0); // Setting the middle square pixels to black (or 0)
+            set_pixel(picture, l, k, 0);
           }
         }
       }
